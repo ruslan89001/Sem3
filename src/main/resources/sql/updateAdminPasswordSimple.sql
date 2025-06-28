9f10c92765921fd842623aa7aadc3ae6b1164727
Nikiforov Ruslan Andreevich <143865928+ruslan89001@users.noreply.github.com>
2025-06-28 22:36:18 +0300

    sem3

--- src/main/resources/sql/updateAdminPasswordSimple.sql
+++
@@ -1,8 +0,0 @@
-UPDATE users 
-SET password = 'admin' 
-WHERE username = 'admin';
-
-UPDATE users SET role = 'ADMIN' WHERE username = 'admin';
-UPDATE users SET role = 'USER' WHERE username != 'admin' AND role = 'user';
-
-SELECT id, username, email, role, password FROM users WHERE username = 'admin'; 
