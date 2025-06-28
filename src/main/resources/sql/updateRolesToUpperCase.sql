cda51272d1393189c3d9d90e1a6a6ca3047a3eba
Nikiforov Ruslan Andreevich <143865928+ruslan89001@users.noreply.github.com>
2025-06-28 22:35:14 +0300

    sem3

--- src/main/resources/sql/updateRolesToUpperCase.sql
+++
@@ -1,4 +0,0 @@
-UPDATE users SET role = 'ADMIN' WHERE username = 'admin';
-UPDATE users SET role = 'USER' WHERE username != 'admin' AND role = 'user';
-
-SELECT id, username, email, role, password FROM users; 
