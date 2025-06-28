9b637ed846d7c5e1919ad60b452961bc7ebf7e95
Nikiforov Ruslan Andreevich <143865928+ruslan89001@users.noreply.github.com>
2025-06-28 22:34:55 +0300

    sem3

--- src/main/resources/sql/updateRolesToUpperCase.sql
+++ src/main/resources/sql/updateRolesToUpperCase.sql
@@ -1,6 +1,4 @@
--- Обновляем роли в верхний регистр
 UPDATE users SET role = 'ADMIN' WHERE username = 'admin';
 UPDATE users SET role = 'USER' WHERE username != 'admin' AND role = 'user';
 
--- Проверяем результат
 SELECT id, username, email, role, password FROM users; 
